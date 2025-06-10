eb4faf75b28e333cb981d09bc3e63964dc8acc4c
rfryrice <yr.francis2g@gmail.com>
2025-06-10 12:39:39 -0700

    testing gpiod v2

--- server/test_chip.py
+++ server/test_chip.py
@@ -1,12 +1,11 @@
 import gpiod
 
-# The main GPIO controller is usually /dev/gpiochip0 on Raspberry Pi 5
-chip = gpiod.Chip('gpiochip0')
+chip = gpiod.Chip('gpiochip0')  # Or use '/dev/gpiochip0'
 
-print(f"Lines for {chip.name} ({chip.label}):")
+print(f"Lines for {chip.path} ({chip.label}):")
 print(f"{'Offset':>6}  {'Name':<16}  {'Direction':<10}  {'Used by'}")
 
-for line in chip.get_all_lines():
+for line in chip:
     info = line.line_info
     offset = info.offset
     name = info.name or ""
